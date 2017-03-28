d3ff39ee8b1e81077b22cef340a1e6a0ab69bf7e
liyunhe <759943432@qq.com>
2017-03-28 17:47:56 +0800

    Initial Commit

---
+++ YHCodeFile/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  YHCodeFile
+//
+//  Created by mrlee on 2017/3/28.
+//  Copyright © 2017年 mrlee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
